b94431879169c164942443d476a2d06f445f8ed0
Santosh Majhee <santoshkumarmajhee3001@gmail.com>
2021-11-09 21:49:37 +0530

    Added Project Files

---
+++ assets/js/main.js
@@ -0,0 +1,33 @@
+// ==================== SHOW MENU ===================
+const navMenu=document.getElementById('nav-menu'),
+    navToggle=document.getElementById('nav-toggle'),
+    navClose=document.getElementById('nav-close');
+
+// ====== MENU SHOW ===========
+// Validate if contsnt exists
+if(navToggle){
+    navToggle.addEventListener('click',()=>{
+        navMenu.classList.add('show-menu')
+    })
+}
+// ============= MENU HIDDEN ====================
+// Validate if constant exists
+if(navClose){
+    navClose.addEventListener("click",()=>{
+        navMenu.classList.remove('show-menu')
+    })
+}
+// =================== REMOVE MENU MOBILE ====================
+const navLink=document.querySelectorAll('.nav__link');
+function linkAction(){
+    const navMenu=document.getElementById('nav-menu')
+    navMenu.classList.remove('show-menu')
+}
+navLink.forEach(n=> n.addEventListener('click',linkAction))
+
+// =================== CHANGE BACKGROUND HEADER =============
+function scrollHeader(){
+    const header=document.getElementById('header');
+    if(this.scrollY>=80) header.classList.add('scroll-header')
+}
+window.addEventListener('scroll',scrollHeader)
