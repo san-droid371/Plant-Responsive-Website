3098ed0beb148693081b0888831c15c8184d983c
Santosh Majhee <santoshkumarmajhee3001@gmail.com>
2021-11-11 23:50:50 +0530

    New features added

--- assets/js/main.js
+++ assets/js/main.js
@@ -30,4 +30,30 @@ function scrollHeader(){
     const header=document.getElementById('header');
     if(this.scrollY>=80) header.classList.add('scroll-header')
 }
-window.addEventListener('scroll',scrollHeader)
+window.addEventListener('scroll',scrollHeader);
+
+// ================= QUESTIONS ACCORDION =================
+const accordionItems=document.querySelectorAll('.questions__item')
+
+accordionItems.forEach((item)=>{
+    const accordionHeader=item.querySelector('.questions__header');
+    accordionHeader.addEventListener('click',()=>{
+        const openItem=document.querySelector(".accordion-open")
+        toggleItem(item)
+        if(openItem && openItem !==item){
+            toggleItem(openItem)
+        }
+    })
+})
+
+const toggleItem= (item)=>{
+    const accordionContent = item.querySelector('.questions__content');
+    if(item.classList.contains('accordion-open')){
+        accordionContent.removeAttribute('style')
+        item.classList.remove('accordion-open')
+    }
+    else{
+        accordionContent.style.height=accordionContent.scrollHeight + 'px'
+        item.classList.add('accordion-open')
+    }
+} 
